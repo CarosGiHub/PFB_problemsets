00dd617444bd6073fbe07b64e98ee3648610efaa
CarosGiHub <carolineharrer@gmx.de>
2025-10-14 17:17:49 -0400

    writewhatthecommitdoes

--- python2.py
+++ python2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#always add the line above so that the script can run smoothly.
 
 # slogan = "XXXXXXXPIRATESXXXX"
 
@@ -44,7 +45,8 @@
 # else:
 #     print("negative")
 
-# #Task 5 
+# #Task 5 add additional if...
+
 # number = -60
 # if number == 0:
 #     print("0")
@@ -69,6 +71,9 @@
 #         print("it is a number that is divisible by three")
 
 #Sys Task
+#important message here: sys.argv[] has string as input. One needs to convert it into integer to make it work
+#let it run a number in the command line 
+#int()
 
 import sys
 
@@ -95,3 +100,35 @@ else:
             print("it is an even number that is smaller than 50")
     if number % 3 == 0:
         print("it is a number that is divisible by three")
+
+
+#if you want to create a script - use the Visual Studio Code - Create New Phyton - Save File (name file here)
+#make sure it is in the right folder/directory
+
+#if a new directory is needed - mkdir command
+
+#always be sure that you work in the right directory 
+#otherwise use cd nameofthedirectory
+#cd PFB_problemsets
+#you can write a few letter of the direction and use the tab function to make it complete the fields
+
+# if I want to carry out the script - I write the following in the terminal 
+#python3 python2(name of the script) 
+#press enter
+
+#if you need to push document from local repository to digital one you need to run git push
+#for this 
+#a) run 
+#git status (gives you information about the status - is there a document that is not synced yet - depicted in red)
+#git add python2.py(the document you would like to add)
+#git commit -m "writewhatthecommitdoes" (no parethesis needed around "writewhatthecommitdoes")
+#if it shows the following that is fine you can ignore it: "hint: The '.git/hooks/pre-commit' hook was ignored because it's not set as executable.
+#hint: You can disable this warning with `git config set advice.ignoredHook false`"
+#git push
+#git status (check if there is not document left that is unassigned. every unassigned document is shown in red)
+
+#Important short cuts
+# command + S: save
+# command + /: makes text in #green
+#tab - adds directories or assigned variables
+#arrow up - helps to find previous commands
