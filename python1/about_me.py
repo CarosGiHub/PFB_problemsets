2abed06fd7b004c43374889adb2797b9f4b6928a
CarosGiHub <carolineharrer@gmx.de>
2025-10-20 11:56:45 -0400

    add multiple folder for for clarity

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
-name = "caroline"
-print("my name is" , name)
+# name = "caroline"
+# print("my name is" , name)
+
+# colour = "blue"
+# print ("my favorite colour is" , colour)
+
+# acitvity = "coding"
+# print ('my favorite activity is' , acitvity)
+
+# animal = "turtle"
+# print ("my favorite animals are" , animal)
+
+# #Try out f"" string formatting.
+
+#print(f"My name: {name} and I love the colour {colour} that contrasts the colour of my favorite animal {animal} really well") 
+
+#Try sys.argv
+import sys
+
+name = sys.argv[1]
+colour = sys.argv[2]
+animal = sys.argv[3]
+
+print (name, " s favorite colour is" , colour, "and her favorite animals are" , animal)
+
